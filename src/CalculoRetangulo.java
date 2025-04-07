dd8b34ce4cdadfa24237f40992acda3cb9b27cc9
Rodolfo-Pessoa <rodolfolujcaspessoa@hotmail.com>
2025-04-07 19:27:17 -0300

    Exercício terminado

--- src/CalculoRetangulo.java
+++ src/CalculoRetangulo.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import Retangulo.Retangulo;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CalculoRetangulo {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+        Retangulo ret = new Retangulo();
+
+        System.out.println("Informe os valores da base e altura do retangulo: ");
+
+        System.out.print("Base: ");
+        ret.base = sc.nextDouble();
+
+        System.out.print("Altura: ");
+        ret.altura = sc.nextDouble();
+
+        System.out.printf("A área do retângulo é: %.2f", ret.areaRetangulo());
+
+        System.out.println("");
+
+        System.out.println("O Perímetro é: ");
+
+        System.out.printf("%.2f%n", ret.perimetro());
+        sc.close();
+
+        System.out.printf("Diagonal %.2f", ret.diagonal());
         }
     }
-}
--- src/Retangulo/Retangulo.java
+++ src/Retangulo/Retangulo.java
@@ -1,4 +1,17 @@
 package Retangulo;
 
 public class Retangulo {
+
+    public double base;
+    public double altura;
+
+    public double areaRetangulo() {
+        return base * altura;
+    }
+    public double perimetro() {
+        return 2 * (base + altura);
+    }
+    public double diagonal() {
+        return Math.sqrt(Math.pow(base,2)+Math.pow(altura,2));
+}
 }
